bd45b968a26adbaaffed338ea8bbd7f4b9896306
DHULFIQAR HAITHAM JASIM <42491711+thotho19@users.noreply.github.com>
2020-02-27 18:09:38 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+data = [['a','b','c'],
+        ['a','b']
+        ]
+
+
+lenSize = len(data[0])
+comparingState = True
+
+for d in data:
+    if lenSize != len(d):
+        same = False
+    else:
+        pass
+print(comparingState)
